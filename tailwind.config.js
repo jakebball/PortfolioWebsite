cfc49ebfadb5c2bb9e44d614d48b9278fa504d51
Jake Silverman <jakebball2014@gmail.com>
2022-05-09 21:06:36 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
